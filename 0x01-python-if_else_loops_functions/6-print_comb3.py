3198940938a61a13e142038363c17f14afb42429
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-09-14 21:04:17 -0700

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,8):
-     for j in range(0,10):
+for i in range(0, 8):
+    for j in range(0, 10):
         if j > i:
             print("{:02d}".format(i * 10 + j), end='')
             print(', ', end='')
