c1b44f0fb18c1fe17d88760e0c155a0e468285af
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-09-17 00:48:16 -0700

    dynamic file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    args[] = list(argv)
-    for i , arg enumerate(args):
-        print("{}: {}".format(i, arg))
+    args = list(argv)
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i , arg in enumerate(args):
+            if i > 0:
+                print("{}: {}".format(i, arg))
