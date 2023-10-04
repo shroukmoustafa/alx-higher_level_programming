1f94a803c3af81037ad73dc9786f0f43b749532c
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-10-04 00:23:50 -0700

    lists

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
     if my_string is None:
         return None
     for i in range(len(my_string)):
-        if ord(my_string[i]) != 67 or ord(my_string[i]) != 99:
+        if ord(my_string[i]) != 67 and ord(my_string[i]) != 99:
             str1.append(my_string[i])
-    return str1
+    return "".join(str1)
