eef46d020ada8f9f33c60ec38d0f052fb2f164b7
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-05-01 18:54:12 -0700

    convert to uppercase letters

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str1 = ""
+    for i in str:
+        if 96 < ord(i) < 123:
+            str1 += chr(ord(i) - 32)
+        else:
+            str1 += i
+    print(str1)
