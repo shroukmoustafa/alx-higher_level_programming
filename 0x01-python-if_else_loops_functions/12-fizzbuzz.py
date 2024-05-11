fd338abf3de14038e183c5d97a3c56d036a286d8
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-05-11 07:45:27 -0700

    m

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,7 +6,7 @@ def fizzbuzz():
         elif (i % 3) == 0:
             print("Fizz", end=" ")
         elif i == 100:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         elif (i % 5) == 0:
             print("Buzz", end=" ")
         else:
