a37a228db3b92a29a4e8a90707f22ad5276430a2
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-05-12 02:05:14 -0700

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    x = []
+    x = sys.argv
+    if len(x) == 1:
+        print("0 arguments")
+    elif len(x) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(len(x) - 1))
+    for i, ar in enumerate(x):
+        if i > 0:
+            print("{}: {}".format(i, ar))
