f406e8a7e012bfe8752ab278cdb060d7c8fe1e27
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-05-12 01:23:10 -0700

    arthemitic operations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1
+    a = 10
+    b = 5
+    print("{} + {} = {:d}".format(a, b, add(a, b)))
+    print("{} - {} = {:d}".format(a, b, sub(a, b)))
+    print("{} * {} = {:f}".format(a, b, mul(a, b)))
+    print("{} / {} = {:f}".format(a, b, div(a, b)))
