30878c7ac8c3754d813c3b00788d6fc8e1c94061
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-05-16 13:49:32 -0700

    lists

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx > (len(my_list) - 1):
+        return None
+    print("Element at index {} is {}".format(idx, my_list.index(idx)))
