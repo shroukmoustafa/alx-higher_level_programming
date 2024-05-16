fcec7d5083e7835ff0cc29d8ec26954cfa755531
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-05-15 17:48:07 -0700

    getting

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4.pyc
+    x = []
+    x = dir(hidden_4.pyc)
+    for name in sorted(x):
+        if not name.startwith('__'):
+            print(name)
