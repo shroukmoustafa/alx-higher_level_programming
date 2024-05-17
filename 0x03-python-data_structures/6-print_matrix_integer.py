e0079d77d478a0aac47c287aa7adb16776b034f4
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-05-17 00:18:44 -0700

    unpacking matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
-            for j in range(len(matrix(i))):
+            for j in range(len(matrix[i])):
                 if j > 0:
                     print(" ", end='')
                 print("{:d}".format(matrix[i][j]), end='')
