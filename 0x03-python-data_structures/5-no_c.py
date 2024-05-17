bde07c4e58eb5df1099498305a6c0d18da04ceee
shroukmoustafa <shroukmoustafa36@gmail.com>
2024-05-16 23:38:50 -0700

    replacing string parts

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        newstring = ""
+        newstring.join(c for c in my_string if (c != 'c' and c != 'C'))
+        return newstring
+    return my_string
